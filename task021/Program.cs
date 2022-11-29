00837d218d199fa590d7f5a2cb6617a0403c09ef
SeleznevAS <seleznevanatserg@yahoo.com>
2022-11-29 21:46:11 +0300

    Task021 edited

--- task021/Program.cs
+++ task021/Program.cs
@@ -1,34 +1,44 @@
-﻿int [] arr = { 1, 5, 4, 3, 2, 6, 7, 1, 1 };
+﻿//Напишите программу, которая принимает на вход координаты двух точек и 
+//находит расстояние между ними в 3D пространстве.
+//A (3,6,8); B (2,1,-7), -> 15.84
+//A (7,-5, 0); B (1,-1,9) -> 11.53
 
-void PrintArray(int[] array)
+
+
+
+Console.WriteLine ("Enter 3D-coordinates fo point A (XYZ)");
+double ax = InPutDoubleNumberTryParse ();
+double ay = InPutDoubleNumberTryParse ();
+double az = InPutDoubleNumberTryParse ();
+Console.WriteLine ("Enter 3D-coordinates fo point B (XYZ)");
+double bx = InPutDoubleNumberTryParse ();
+double by = InPutDoubleNumberTryParse ();
+double bz = InPutDoubleNumberTryParse ();
+
+
+
+double distance = DistanceBetweenTwoPointIn3DSpace(ax,ay,az,bx,by,bz);
+
+Console.WriteLine ($"Distance between points A and B = {distance}");
+
+double InPutDoubleNumberTryParse ()
 {
-int count = array.Length;
+    Console.WriteLine("Enter number, please.");
+    bool isParsed = double.TryParse(Console.ReadLine(), out double Num);
 
-for (int i = 0; i < count; i++)
-{
-    Console.Write($"{array[i]} ");
-}
-Console.WriteLine();
-}
-
-
-
-void SelectionSort(int[] array)
-{
-for (int i = 0; i < array.Length - 1 ; i++)
-{
-int minPosition = i;
-
-    for (int j = i + 1; j < array.Length; j++)
+    if (!isParsed)
     {
-        if(array[j] < array[minPosition]) minPosition = j;
+        Console.WriteLine("Sorry, but You entered bullshit. Please, number enter.");
+        return -1;
     }
-    int temporary = array[i];
-    array[i] = array[minPosition];
-    array[minPosition] = temporary;
+    else
+    {
+        return Num;
+    }
 }
-}
-PrintArray(arr);
-SelectionSort(arr);
+double DistanceBetweenTwoPointIn3DSpace (double ax, double ay, double az, double bx, double by, double bz)
+{
+    double result = Math.Sqrt(Math.Pow((ax-bx),2) + Math.Pow((ay-by),2) + Math.Pow((az-bz),2));
 
-PrintArray(arr);
+    return result;
+}
