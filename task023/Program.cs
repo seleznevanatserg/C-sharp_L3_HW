012395ebb89a6ef1977cd54132136c83215a7717
SeleznevAS <seleznevanatserg@yahoo.com>
2022-11-29 21:52:51 +0300

    Task023 edited

--- task023/Program.cs
+++ task023/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27    5 -> 1, 8, 27, 64, 125
+// массив
+
+int num = InputIntNumberTryParse();
+int [] cubesTable = GetTableOfCubes(num);
+PrintArray (cubesTable);
+
+int InputIntNumberTryParse ()
+{
+    Console.WriteLine("Enter number, please.");
+    bool isParsed = int.TryParse(Console.ReadLine(), out int Num);
+
+    if (!isParsed)
+    {
+        Console.WriteLine("Sorry, but You entered bullshit. Please, number enter.");
+        return -1;
+    }
+    else
+    {
+        return Num;
+    }
+}
+int [] GetTableOfCubes (int num)
+{
+    int [] arr = new int [num];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = (int)(Math.Pow(i+1, 3));
+    }
+    return arr;
+}
+void PrintArray (int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
